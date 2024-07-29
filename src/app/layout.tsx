2dd3de60846e248844f7e201ab2700c00ff2ccca
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2024-07-29 12:32:07 +0200

    Added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <html lang="en">
       <body className={` ${inter.className } m-0 p-0 w-screen`}>
@@ -31,25 +30,28 @@ export default function RootLayout({
           {children}
           <Footer />
         </ConvexClientProvider>
-
-        <Script
-          id="fb-pixel"
+        <Script 
+          id="facebook-pixel"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-          __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '492793676571323');
-          fbq('track', 'PageView');
-          `,
-          }}
-        />
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '492793676571323');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display:"none" }}
+            src="https://www.facebook.com/tr?id=492793676571323&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
